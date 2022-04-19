8cce62fe4ec13673e8b0ac227ac84f3b02c13d53
Kayzu <96322205+Kayzyu@users.noreply.github.com>
2022-04-19 20:00:11 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -7,35 +7,24 @@
 
 import sys
 from importlib import import_module
-
-import requests
-from telethon.tl.functions.channels import InviteToChannelRequest as Addbot
+from pytgcalls import idle
 
 from userbot import (
     BOTLOG_CHATID,
-    BOT_USERNAME,
     BOT_TOKEN,
     BOT_VER,
     LOGS,
-    kyyblacklist,
     bot,
-    call_py,
 )
 from userbot.modules import ALL_MODULES
-from userbot.utils import autobot
+from userbot.clients import kay_ubot_on, multiclientkay
+from userbot.utils import autobot, autopilot, git
 
 try:
-    bot.start()
-    call_py.start()
-    user = bot.get_me()
-    kyyblacklist = requests.get(
-        "https://raw.githubusercontent.com/muhammadrizky16/Kyyblack/master/kyyblacklist.json"
-    ).json()
-    if user.id in kyyblacklist:
-        LOGS.warning(
-            "MAKANYA GA USAH BERTINGKAH GOBLOK, USERBOTnya GUA MATIIN NAJIS BANGET DIPAKE ORANG KEK LU.\nCredits: @Kayzuuuuu"
-        )
-        sys.exit(1)
+    client = multiclientkay()
+    total = 5 - client
+    git()
+    LOGS.info(f"Total Clients = {total} User")
 except Exception as e:
     LOGS.info(str(e), exc_info=True)
     sys.exit(1)
@@ -43,30 +32,19 @@ except Exception as e:
 for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
 
-LOGS.info(
-    f"Jika {user.first_name} Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/KayzuSupport")
-LOGS.info(
-    f"✨Kay-Userbot✨ ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
-
-
-async def check_alive():
-    try:
-        if BOTLOG_CHATID != 0:
-            await bot.send_message(BOTLOG_CHATID, "✨ **Kay Userbot Berhasil Diaktifkan**!!\n━━━━━━━━━━━━━━━\n➠ **Userbot Version** - 3.1.0@Kay-Userbot\n➠ **Ketik** `.ping` **Untuk Mengecheck Bot**\n━━━━━━━━━━━━━━━\n➠ **Powered By:** @KayXChannel ")
-    except Exception as e:
-        LOGS.info(str(e))
-    try:
-        await bot(Addbot(int(BOTLOG_CHATID), [BOT_USERNAME]))
-    except BaseException:
-        pass
-
-bot.loop.run_until_complete(check_alive())
+bot.loop.run_until_complete(kay_ubot_on())
+if not BOTLOG_CHATID:
+    bot.loop.run_until_complete(autopilot())
 if not BOT_TOKEN:
     LOGS.info(
         "BOT_TOKEN Vars tidak terisi, Memulai Membuat BOT Otomatis di @Botfather..."
     )
     bot.loop.run_until_complete(autobot())
-
+LOGS.info(
+    f"Jika Anda Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/KayzuSupport")
+LOGS.info(
+    f"✨Kay-Userbot✨ ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
+idle()
 if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
 else:
